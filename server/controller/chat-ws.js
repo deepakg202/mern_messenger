2c63aa0f0bd906f1f92a1cfc53ff844e6ea8816c
Deepak Gupta <deepakgupta191199@gmail.com>
2021-04-22 23:10:58 +0530

    chat ui

--- server/controller/chat-ws.js
+++ server/controller/chat-ws.js
@@ -2,9 +2,16 @@
 const chatWs = (expressApp) => {
   const server = require('http').createServer(expressApp)
   const io = require('socket.io')(server)
-
-  io.on('connection', socket => {
+  io.of('/chat').on('connection', socket => {
     console.log('Some client connected')
+
+    socket.on('send-message', message => {
+      socket.emit('new-message', message)
+    })
+    socket.on('disconnect', () => {
+      console.log("disconnected")
+    })
+  
   })
 
   return server
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,21 +3,50 @@ import {useSelector} from "react-redux";
 import socketIOClient from "socket.io-client";
 
 function Chat() {
-  const {sessionInfo} = useSelector(state => state.session)
-  const [response, setResponse] = useState(sessionInfo.username);
+  
+  const [messages, setMessages] = useState([]) 
+  const {username} = useSelector((state) => state.session.sessionInfo)
+  const socket = socketIOClient("/chat")
 
   useEffect(() => {
-    const socket = socketIOClient("/ws/chat");
-    socket.on("FromAPI", data => {
-      setResponse(data);
+    socket.on("new-message", message => {
+      setMessages((msgs) => [...msgs, message])
     });
-    return () => socket.disconnect();
+    return () => socket.disconnect()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const chatHandler = (e) => {
+    e.preventDefault();
+    const msg = e.target.elements["message"].value;
+    if(msg)
+      socket.emit("send-message", {msg: msg, author: username})
+  }
+
+  const Message = ({msg, author}) => {
+
+    return <div className="text-start text-white bg-warning">{author}: {msg}</div>
+  }
+
 
   return (
     <div className="container my-3">
-      It's <div>{response}</div>
-    </div>
+      <div className="h2">Global Chat</div>
+      <div id="chatSection">
+        <div id="chat-window" className="overflow-y-auto bg-dark my-2">
+          {
+            messages.map((m,i) => <Message key={i} msg={m.msg} author={m.author}/>)
+          }
+        </div>
+        <form className="chat-form" onSubmit={chatHandler}>
+          <label className="form-label">
+            Enter a message:
+            <input type="text" name="message" className="form-control" />
+          </label>
+          <button type="submit" className="mx-2 btn btn-primary">Submit</button>
+        </form>
+      </div>
+     </div>
   );
 }
 
